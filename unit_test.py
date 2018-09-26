420e05f0e948ce3791a721082c050f161a2d32b1
Al (Office) <alan.colburn@csulb.edu>
2018-09-26 11:31:23 -0700

    add console-based UI

--- unit_test.py
+++ unit_test.py
@@ -1,6 +1,40 @@
 from stat_test import *
 
+s = '''
+o - I want to start over.
+2 - I am comparing two groups
+3 - I am comparing three or more groups
+i - My groups are independent
+m - My groups are matched or dependent
+p - My data is parametric
+n - My data is nonparametric
+c - My data includes a covariate
+q - I am done. Let\'s quit!
+Your selection: '''
+
+
+def processSelection(selection):
+    if (selection == 'o'):
+        makeList()
+    elif (selection == '2'):
+        isTwoGroups()
+    elif (selection == '3'):
+        isNotTwoGroups()
+    elif (selection == 'i'):
+        isNotPaired()
+    elif (selection == 'm'):
+        isPaired()
+    elif (selection == 'p'):
+        isParametric()
+    elif (selection == 'n'):
+        isNotParametric()
+    elif (selection == 'c'):
+        hasCovariate()
+
+
 makeList()
-isParametric()
-isNotTwoGroups()
-printList()
+selection = ''
+while (selection != 'q'):
+    selection = input(s)
+    processSelection(selection)
+    printList()
